8351a30d0e4a51f6caee8fa80a9ca0c011061a09
rajs <rajs@nrifintech.com>
2019-04-17 16:39:00 +0530

    Removed redudent file

--- inf/module-register.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(appObject,moduleObjects){
-    moduleObjects.forEach(module => {
-        module.routes.forEach(route =>{
-            appObject.use(route.routePath,route.routeFunction)
-        })
-    });
-}
